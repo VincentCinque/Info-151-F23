170425ac06c5414a5fb63f35849e795aaeb08584
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2023-11-14 22:13:28 -0500

    working JS

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,23 @@ var mainNav = document.querySelector ("#mainNav");
 var burgerBtn = document.querySelector("#burger button")
 console.log(burgerBtn);
 
+var canYouSeeMainNav = false;
+
 //select all section tags in the html document, add then to the sectionTags variable 
 //var sectionTags = document.querySelectorAll("section");
 
 //function is for repeadly calling a repetative task, also just like python
 function showNav(){
     console.log("button clicked");
+    if(canYouSeeMainNav === false){
     mainNav.classList.add("showmainNav");
+    canYouSeeMainNav = true;
+
+}
+else{
+    mainNav.classList.remove("showmainNav");
+    canYouSeeMainNav = false;
+}
 }
 
 burgerBtn.addEventListener("click", showNav);
