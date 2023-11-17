35a9294145bf28d4df33c2e156b07fcab0a88c31
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2023-11-17 12:25:48 -0500

    styling complete

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,32 @@
 
 var mainNav = document.querySelector ("#mainNav");
 
+var btn = document.querySelector (".buttons")
+
 var burgerBtn = document.querySelector("#burger button")
 console.log(burgerBtn);
 
 var canYouSeeMainNav = false;
+var canYouSeeMainBtn = false;
 
 //select all section tags in the html document, add then to the sectionTags variable 
 //var sectionTags = document.querySelectorAll("section");
 
 //function is for repeadly calling a repetative task, also just like python
+function showBtn(){
+    console.log("button clicked");
+if(canYouSeeMainBtn === false){
+    btn.classList.add("showbtn")
+    canYouSeeMainBtn = true;
+
+}
+else{
+    btn.classList.remove("showbtn")
+    canYouSeeMainBtn = false;
+}
+}
+
+
 function showNav(){
     console.log("button clicked");
     if(canYouSeeMainNav === false){
@@ -23,7 +40,7 @@ else{
     canYouSeeMainNav = false;
 }
 }
-
+burgerBtn.addEventListener("click", showBtn);
 burgerBtn.addEventListener("click", showNav);
 //this calls a function, just like python
 //showNav()
