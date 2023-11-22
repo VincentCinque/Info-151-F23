f160eb8880b65efb08d4ce1c9355a8fd6eda4b91
VincentCinque <146038947+VincentCinque@users.noreply.github.com>
2023-11-22 16:26:15 -0500

    added modal (needs some fixing)

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,29 @@
+// navigation
+
 
 
 var mainNav = document.querySelector ("#mainNav");
 var burgerBtn = document.querySelector("#burger button")
 console.log(burgerBtn);
+var signInUpButtons = document.querySelector("#sign-in-up-buttons")
 
 var canYouSeeMainNav = false;
+var canYouSeeButtons = false;
 
 //select all section tags in the html document, add then to the sectionTags variable 
 //var sectionTags = document.querySelectorAll("section");
-
 //function is for repeadly calling a repetative task, also just like python
 function showNav(){
     console.log("button clicked");
     if(canYouSeeMainNav === false){
     mainNav.classList.add("showmainNav");
+    signInUpButtons.classList.add("showSignIn");
     canYouSeeMainNav = true;
 
 }
 else{
     mainNav.classList.remove("showmainNav");
+    signInUpButtons.classList.remove(showSignIn)
     canYouSeeMainNav = false;
 }
 }
@@ -27,3 +32,26 @@ burgerBtn.addEventListener("click", showNav);
 //this calls a function, just like python
 //showNav()
 
+
+//Signin
+
+var signInBtn = document.querySelector ("#sign-in");
+var signInContainer = document.querySelector ("#signin-container");
+var closeSignUpModalBtn = document.querySelector ("#close-sign-up-modal-btn");
+
+function showSignInModal() {
+    //console.log("click show");
+    signInContainer.classList.add("show-signIn-container")
+
+    signInUpButtons.classList.remove(showSignIn)
+
+}
+
+function hideSignInModal(){
+    signInContainer.classList.remove("show-signIn-container")
+    //hide the navigation on open
+    mainNav.classList.remove("showmainNav");
+}
+
+signInBtn.addEventListener("click", showSignInModal);
+closeSignUpModalBtn.addEventListener("click", hideSignInModal);
